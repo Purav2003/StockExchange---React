a59c4f35b186e743f48cddcfc25d1fb142103f2b
Purav2003 <puravshah20@gnu.ac.in>
2023-01-13 23:13:22 +0530

    Updated Overview Page

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,8 +1,12 @@
 import { StockList } from "../componenets/StockList"
 import { AutoComplete } from "../componenets/AutoComplete"
+import logo from "./logo.png"
 
 export const StockOverviewPage = () =>{
     return <div>
+        <div className="d-flex" style={{marginLeft:'450px'}}>
+            <div style={{height:'50px'}}><img src={logo} className="text-center"></img></div>
+            <div style={{height:'50px',marginTop:'75px',marginLeft:'-60px',fontFamily:'Raleway'}}><h3>Stock Search</h3></div></div>
         <AutoComplete />
         <StockList />
     </div>
